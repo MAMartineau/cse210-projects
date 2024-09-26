08e0b82d78b718b55f0b3cf3e7e4fade4f9b79c6
Austin Martineau <m.austin.martineau@gmail.com>
2024-09-25 23:56:37 -0600

    fully completed assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int userNumber = PromptUserNumber();
+
+        int numberSquared = SquareNumber(userNumber);
+
+        DisplayResult(userName, numberSquared);
+
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");    
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int numberSquared)
+    {
+        Console.WriteLine($"{name}, the square of your number is {numberSquared}");
+    }
+
+
+
 }
