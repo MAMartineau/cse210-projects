42f804f5fd2fcac7775e84e4bdd37545d0a610b1
Austin Martineau <m.austin.martineau@gmail.com>
2024-11-12 00:01:08 -0700

    w01 prove assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Step 1: Ask the user for the magic number
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Step 3: Generate a random number as the magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("Guess the number I am thinking of (between 1 and 100)");
+
+        int userGuess = 0;
+
+        // Step 2: Add a loop to keep asking until the guess is correct
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
