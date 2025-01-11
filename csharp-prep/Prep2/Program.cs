93f325830825b5c818746ee9fda2d5d9479623cf
Austin Martineau <m.austin.martineau@gmail.com>
2025-01-11 16:02:14 -0700

    prep 2 learning activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+
+        int gradePercent = int.Parse(valueFromUser);
+        string letter;
+        
+        if (gradePercent >= 90)
+        {
+           letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+           letter = "F";
+        }
+        
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations on passing the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this class. Try again.");
+            Console.WriteLine("You have what it takes to pass. You can do this!");
+        }
     }
 }
